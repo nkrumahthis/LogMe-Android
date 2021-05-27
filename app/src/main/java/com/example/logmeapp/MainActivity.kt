de2f5b9670e2eb1bbfb9fa883da68cbb3ca222c2
nkrumahinc <nkrumahthis@gmail.com>
2021-05-27 19:09:39 +0000

    counter

--- app/src/main/java/com/example/logmeapp/MainActivity.kt
+++ app/src/main/java/com/example/logmeapp/MainActivity.kt
@@ -2,15 +2,24 @@ package com.example.logmeapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import kotlinx.android.synthetic.main.activity_main.*
 
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var logger:Logme
+    private var count:Int = 0
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        tvDisplay.text = count.toString()
+        btCount.setOnClickListener(View.OnClickListener {
+            count++;
+            tvDisplay.text = count.toString()
+        })
 
         logger = Logme(this@MainActivity, "DEBUGGING", "Counter", "nkrumahthis");
 
